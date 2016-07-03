d19c89969d2585063e16544adee065c67e717def
Markus Mayer <widemeadows@gmail.com>
2016-07-03 19:05:47 +0200

    random walk example

--- acf_plot.m
+++ acf_plot.m
@@ -1,7 +1,27 @@
 signal_gen
 
+t_max = 2;
+N = 100;
+
+t = linspace(0, t_max, N);
+x = 1:numel(t);
+
+% AR(1) / random walk
+phi = [1]; %[0.5 0.3 -1];
+y = nan(size(t));
+y(1) = 42;
+for i=2:numel(t)
+    for lag=1:numel(phi)
+        lagged_y = 0;
+        if i-lag > 0
+            lagged_y = y(i-lag);
+        end
+        y(i) = 0.5*randn(1) + phi(lag)*lagged_y;
+    end
+end
+
 % determine the autocorrelation
-acf = []
+acf = [1];
 for i=x
     acf = [acf; my_corr(y,y,i)];
 end
@@ -10,9 +30,12 @@ end
 pacf = my_pacf(y, N);
 
 % check against the MATLAB function
-acf_sys = xcorr(y, N, 'coeff');
-acf_sys = acf_sys(N+1:end-1);
+%acf_sys = xcorr(y, N, 'coeff');
+%acf_sys = acf_sys(N+1:end-1);
 
+%pacf_sys = parcorr(y, N-1);
+
+close all;
 figure;
 subplot(3,1,1);
 plot(t, y, 'LineWidth', 2);
@@ -22,16 +45,16 @@ ylabel('signal');
 grid on;
 
 subplot(3,1,2);
-plot(t,acf, 'LineWidth', 2); hold on;
-plot(t,acf_sys, 'r:', 'LineWidth', 2)
+stem(t,acf(1:end-1), 'LineWidth', 2); hold on;
+%stem(t,acf_sys, 'r:', 'LineWidth', 2)
 title('autocorrelation (ACF)');
 xlabel('lag [s]');
 ylabel('coefficient')
 grid on;
 
 subplot(3,1,3);
-plot(t, pacf(1:end-1), 'LineWidth', 2); hold on;
-%plot(t,acf_sys, 'r:', 'LineWidth', 2)
+stem(t, pacf(1:end-1), 'LineWidth', 2); hold on;
+%stem(t, pacf_sys, 'r:', 'LineWidth', 2)
 title('partial autocorrelation (PACF)');
 xlabel('lag [s]');
 ylabel('coefficient')
