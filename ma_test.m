8e4668fb0d2866bcae7cb0658947a0661c4f8c06
Markus Mayer <widemeadows@gmail.com>
2016-07-06 00:58:55 +0200

    added an MA(2) example

--- ma_test.m
+++ ma_test.m
@@ -6,10 +6,14 @@ t_max = 2;
 N = 100;
 
 t = linspace(0, t_max, N);
-x = 1:numel(t);
+x = 0:numel(t);
 
 % MA(1)
-theta = [0.8];
+%theta = [0.8];
+
+% MA(2) -- https://onlinecourses.science.psu.edu/stat510/node/48
+theta = [1 0.7];
+
 ma_degree = numel(theta);
 
 mean = -42;
@@ -37,7 +41,7 @@ for d=1:num_differentiations
 end
 
 % determine the autocorrelation
-acf = [1];
+acf = [];
 for i=x
     acf = [acf; my_corr(y,y,i)];
 end
@@ -70,11 +74,11 @@ in_confidence = abs(acf) >= confidence_interval;
 out_confidence = ~in_confidence;
 
 subplot(3,1,2);
-stem(t(out_confidence),acf(out_confidence), 'LineWidth', 2, 'Color', [0.7 0.7 1]); hold on;
-stem(t(in_confidence),acf(in_confidence), 'LineWidth', 2); hold on;
+stem(x(out_confidence),acf(out_confidence), 'LineWidth', 2, 'Color', [0.7 0.7 1]); hold on;
+stem(x(in_confidence),acf(in_confidence), 'LineWidth', 2); hold on;
 %stem(t,acf_sys, 'r:', 'LineWidth', 2)
 title('autocorrelation (ACF)');
-xlabel('lag [s]');
+xlabel('lag');
 ylabel('coefficient')
 grid on;
 
@@ -83,10 +87,10 @@ in_confidence = abs(pacf) >= confidence_interval;
 out_confidence = ~in_confidence;
 
 subplot(3,1,3);
-stem(t(out_confidence),pacf(out_confidence), 'LineWidth', 2, 'Color', [0.7 0.7 1]); hold on;
-stem(t(in_confidence),pacf(in_confidence), 'LineWidth', 2); hold on;
+stem(x(out_confidence),pacf(out_confidence), 'LineWidth', 2, 'Color', [0.7 0.7 1]); hold on;
+stem(x(in_confidence),pacf(in_confidence), 'LineWidth', 2); hold on;
 %stem(t, pacf_sys, 'r:', 'LineWidth', 2)
 title('partial autocorrelation (PACF)');
-xlabel('lag [s]');
+xlabel('lag');
 ylabel('coefficient')
 grid on;
