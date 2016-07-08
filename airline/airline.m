2850cd586508e8af7fcf4568d3e5d68b6930c1b9
Markus Mayer <widemeadows@gmail.com>
2016-07-08 23:31:00 +0200

    also clipping the ACF plots

--- airline/airline.m
+++ airline/airline.m
@@ -1,8 +1,10 @@
+% attempting to follow the StackExchange answer http://stats.stackexchange.com/a/9017/26843
+
 clear all;
 close all;
 addpath '..';
 
-MAX_DISPLAYED_PACF_LAGS=60;
+MAX_DISPLAYED_LAGS=40;
 
 % read in the airline series
 fileID = fopen('international-airline-passengers.csv', 'r');
@@ -35,25 +37,25 @@ xlabel('month');
 ylabel('\Deltac(t) \cdot 10^{-3}');
 title('change of airline passengers');
 
-lags = 0:N;
+lags = 0:MAX_DISPLAYED_LAGS;
 acf = my_corr(double(passengers), double(passengers), lags);
 dacf = my_corr(double(d_passengers), double(d_passengers), lags);
 
-pacf = my_pacf(double(passengers), N);
-dpacf = my_pacf(double(d_passengers), N);
+pacf = my_pacf(double(passengers), MAX_DISPLAYED_LAGS);
+dpacf = my_pacf(double(d_passengers), MAX_DISPLAYED_LAGS);
 
 % confidence bands for 95% confidence of the null hypothesis (i.e. no correlation)
 % https://en.wikipedia.org/wiki/Correlogram
 % https://en.wikipedia.org/wiki/1.96
-PACF_conf_x = [lags(1:MAX_DISPLAYED_PACF_LAGS+1), fliplr(lags(1:MAX_DISPLAYED_PACF_LAGS+1))];
-PACF_conf_y = [ones(1,MAX_DISPLAYED_PACF_LAGS+1)*1.96/sqrt(N), fliplr(-ones(1,MAX_DISPLAYED_PACF_LAGS+1))*1.96/sqrt(N)];
+PACF_conf_x = [lags, fliplr(lags)];
+PACF_conf_y = [ones(size(lags))*1.96/sqrt(N), fliplr(-ones(size(lags)))*1.96/sqrt(N)];
 
 % confidence bands for 95% confidence of the null hypothesis (i.e. no correlation)
 % the ACF confidence bands are using the Bartlett approximation describe on
 % https://en.wikipedia.org/wiki/Correlogram
-bartlett_conf = zeros(1, N);
+bartlett_conf = zeros(1, MAX_DISPLAYED_LAGS+1);
 acf_squared = acf.^2;
-for i=1:N
+for i=1:MAX_DISPLAYED_LAGS
     bartlett_conf(i+1) = 1.96*sqrt( (1 + 2*sum(acf_squared(2:i))) / N);
 end
 
@@ -67,28 +69,30 @@ stem(lags, acf, ':o', 'filled', 'MarkerSize', 3);
 title('Sample ACF c(t)');
 xlabel('lag')
 ylabel('\rho( k)')
-xlim([0 N]);
+xlim([0 MAX_DISPLAYED_LAGS]);
 
 hold on 
 fill(ACF_conf_x, ACF_conf_y, 1,...
         'FaceColor', [0 0 0], ...
         'EdgeColor', 'none', ...
         'FaceAlpha', 0.1);
-hold off 
+
+% send the filled are to the background
+set(gca, 'children', flipud(get(gca, 'children')));
 
 subplot(2,2,3);
-stem(lags(1:MAX_DISPLAYED_PACF_LAGS), pacf(1:MAX_DISPLAYED_PACF_LAGS), ':o', 'filled', 'MarkerSize', 3);
+stem(lags(1:MAX_DISPLAYED_LAGS), pacf(1:MAX_DISPLAYED_LAGS), ':o', 'filled', 'MarkerSize', 3);
 title('Sample PACF c(t)');
 xlabel('lag k')
 ylabel('\alpha(k)')
-xlim([0 MAX_DISPLAYED_PACF_LAGS]);
+xlim([0 MAX_DISPLAYED_LAGS]);
 
 hold on 
 fill(PACF_conf_x, PACF_conf_y, 1,....
         'FaceColor', [0 0 0], ...
         'EdgeColor', 'none', ...
         'FaceAlpha', 0.1);
-hold off 
+set(gca, 'children', flipud(get(gca, 'children')));
 
 % The autocorrelogram of the differentiated time series
 % shows a strong seasonality of 12 lags (i.e. months).
@@ -99,25 +103,25 @@ stem(lags, dacf, ':o', 'filled', 'MarkerSize', 3);
 title('Sample ACF \Deltac(t)');
 xlabel('lag')
 ylabel('\rho( k)')
-xlim([0 N]);
+xlim([0 MAX_DISPLAYED_LAGS]);
 
 hold on 
 fill(ACF_conf_x, ACF_conf_y, 1,....
         'FaceColor', [0 0 0], ...
         'EdgeColor', 'none', ...
         'FaceAlpha', 0.1);
-hold off 
+set(gca, 'children', flipud(get(gca, 'children')));
 
 subplot(2,2,4);
-stem(lags(1:MAX_DISPLAYED_PACF_LAGS), dpacf(1:MAX_DISPLAYED_PACF_LAGS), ':o', 'filled', 'MarkerSize', 3);
+stem(lags(1:MAX_DISPLAYED_LAGS), dpacf(1:MAX_DISPLAYED_LAGS), ':o', 'filled', 'MarkerSize', 3);
 title('Sample PACF \Deltac(t)');
 xlabel('lag k')
 ylabel('\alpha(k)')
-xlim([0 MAX_DISPLAYED_PACF_LAGS]);
+xlim([0 MAX_DISPLAYED_LAGS]);
 
 hold on 
 fill(PACF_conf_x, PACF_conf_y, 1,....
         'FaceColor', [0 0 0], ...
         'EdgeColor', 'none', ...
         'FaceAlpha', 0.1);
-hold off
+set(gca, 'children', flipud(get(gca, 'children')));
